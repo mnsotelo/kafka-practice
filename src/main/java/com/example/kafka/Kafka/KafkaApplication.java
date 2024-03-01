6ee74c2438e144563a677be3cb57fff319d55536
Martin Sotelo <martinsoteloferry.inf3@gmail.com>
2024-03-01 22:55:47 +0100

    First commit

--- src/main/java/com/example/kafka/Kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/Kafka/KafkaApplication.java
@@ -1,19 +1,8 @@
 package com.example.kafka.Kafka;
 
-import com.github.javafaker.Faker;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
-import reactor.util.function.Tuple2;
 
-import java.time.Duration;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class KafkaApplication {
@@ -23,5 +12,4 @@ public class KafkaApplication {
 	}
 
 
-}
 }
